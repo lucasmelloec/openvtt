d70cec90fb0afe7fab7529e9e0063b468581aba5
Lucas Mello <lucasmelloec@gmail.com>
2023-09-13 18:05:41 -0300

    wip: jwt

--- openvtt_server/src/routes/auth.rs
+++ openvtt_server/src/routes/auth.rs
@@ -1,6 +1,8 @@
 use axum::{extract::State, response::Json, routing::post, Router};
 use diesel::prelude::*;
-use serde::Deserialize;
+use jsonwebtoken::{encode, EncodingKey, Header};
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
 
 use crate::{
     database::*,
@@ -19,10 +21,16 @@ struct AuthPayload {
     password: String,
 }
 
+#[derive(Serialize, Deserialize)]
+struct Claims {
+    sub: String,
+    exp: usize,
+}
+
 async fn login(
     State(database_pool): State<DatabasePool>,
     Json(payload): Json<AuthPayload>,
-) -> Result<Json<models::Player>, AppError> {
+) -> Result<Json<Value>, AppError> {
     use crate::database::schema::players::dsl::*;
 
     if payload.username.is_empty() || payload.password.is_empty() {
@@ -40,8 +48,15 @@ async fn login(
     if !bcrypt::verify(&payload.password, &player.hashed_password).unwrap() {
         return Err(AuthError::InvalidCredentials.into());
     }
+    let claims = Claims {
+        sub: player.username.to_owned(),
+        exp: 2000000000,
+    };
+    let secret = "so secretive";
+    let key = EncodingKey::from_secret(secret.as_bytes());
+    let token = encode(&Header::default(), &claims, &key).unwrap();
 
-    Ok(Json(player))
+    Ok(Json(json!({"access_token": token})))
 }
 
 async fn logout() {}
